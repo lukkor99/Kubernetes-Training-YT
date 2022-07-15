aab261fb2ea2f052f82d2114da1c22f0ee02e246
lukkor99 <lukasz.kordas1@student.up.krakow.pl>
2022-07-15 14:40:29 +0200

    Deployment details edit in .yml file

--- scripts/kubectl_commands.sh
+++ scripts/kubectl_commands.sh
@@ -10,8 +10,10 @@ kubectl get pod # checking all pods in current namespace
 kubectl create deployment ngnix-deployment --image=ngnix # creating a basic deployment named ngnix deployemnt with latest ngnix as a base image
 
 kubectl get deployement # preview of created deployment
-kubectl get pod # get latest pod 
+kubectl get pod # get latest pod - abstraction of the container
 kubectl get replicaset # get replicaset - management unit of the pod replicas
+
+kubectl edit deployment ngnix-deployment >> scripts/outputs/ngnix-config
 
 
 
