2ba2a9775ea2a5d0870fc8673ec7ff1467ec70c1
lukkor99 <lukasz.kordas1@student.up.krakow.pl>
2022-07-18 15:58:04 +0200

    Setup script refactor - mongo config map command added

--- mongo-db-project/mongo_setup.sh
+++ mongo-db-project/mongo_setup.sh
@@ -6,6 +6,8 @@ echo -n 'to be encrypted' | base64 # create encoded credentials
 
 kubectl get secret mongodb-secret # retrieve created secret 
 
+kubectl apply -f config/mongo-configmap.yaml # create config map for mongo db express 
+
 kubectl apply -f config/mongo-deployment.yaml # create deployment for mongodb
 
 kubectly apply -f config/mongoexpress-deployment.yaml # create deployment for mongo express
