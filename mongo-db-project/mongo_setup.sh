a47d3114e2cc2843ca8a4ac36a79f556c6c747bb
lukkor99 <lukasz.kordas1@student.up.krakow.pl>
2022-07-18 15:57:02 +0200

    Mongo db services setup ready with all yaml configuration and script

---
+++ mongo-db-project/mongo_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+kubectl apply -f config/mongo-secret.yaml # creating mongo secret (before deployment!!!)
+
+echo -n 'to be encrypted' | base64 # create encoded credentials
+
+kubectl get secret mongodb-secret # retrieve created secret 
+
+kubectl apply -f config/mongo-deployment.yaml # create deployment for mongodb
+
+kubectly apply -f config/mongoexpress-deployment.yaml # create deployment for mongo express
+
+minikube service mongo-express-service # assign external ip to access the service
+
+exit 0
