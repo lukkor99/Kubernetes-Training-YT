6dff10385f736d91849e9b85151ce4a3a276344f
lukkor99 <lukasz.kordas1@student.up.krakow.pl>
2022-07-19 13:57:26 +0200

    Namespace adjusted and created (mongo_setup.sh)

--- mongo-db-project/mongo_setup.sh
+++ mongo-db-project/mongo_setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-kubectl apply -f config/mongo-secret.yaml # creating mongo secret (before deployment!!!)
+kubectl create namespace mongo-project
+
+kubectl apply -f mongo-db-project/config/mongo-secret.yaml # creating mongo secret (before deployment!!!)
 
 echo -n 'to be encrypted' | base64 # create encoded credentials
 
 kubectl get secret mongodb-secret # retrieve created secret 
 
-kubectl apply -f config/mongo-configmap.yaml # create config map for mongo db express 
+kubectl apply -f mongo-db-project/config/mongo-configmap.yaml # create config map for mongo db express 
 
-kubectl apply -f config/mongo-deployment.yaml # create deployment for mongodb
+kubectl apply -f mongo-db-project/config/mongo-deployment.yaml # create deployment for mongodb
 
-kubectly apply -f config/mongoexpress-deployment.yaml # create deployment for mongo express
+kubectl apply -f mongo-db-project/config/mongoexpress-deployment.yaml # create deployment for mongo express
 
 minikube service mongo-express-service # assign external ip to access the service
 
