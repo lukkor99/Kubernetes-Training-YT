f7ad07c9c954e7db0ed89c915d9a10be115a2d71
lukkor99 <lukasz.kordas1@student.up.krakow.pl>
2022-07-19 14:58:49 +0200

    https protocol enabled via secret and ingress

---
+++ scripts/ingress.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+kubectl get ns # check for existing namespaces
+
+kubectl get all -n kubernetes-dashboard # list all components within namespace
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=foo.bar.com"
+
+kubectl create secret tls dashboard-tls-secret --key="tls.key" --cert="tls.crt"
+
+kubectl get secret dashboard-tls-secret -o yaml >> ingress/dashboard-tls-secret.yaml
+
+kubectl apply -f ingress/dashboard-ingress.yaml # create ingress from yaml configuration file
+
+kubectl describe ingress k8s-dasboard -n kubernetes-dashboard # detailed backenmd info of specefied ingress
+
+exit 0
